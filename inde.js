0c47f2e8591c1d1686910cbd9dc0d15faff8d079
= <vetaly2014@gmail.com>
2021-06-12 15:27:54 +0300

    HomeWork11

---
+++ inde.js
@@ -0,0 +1,39 @@
+let links = document.querySelector('#links');
+let divCol = document.querySelector('#div');
+let mark =document.querySelector('#mark')
+
+// let bgColor = prompt('Введіть фон для сторінки')
+// document.body.style.backgroundColor = bgColor;
+
+// let fontStyle = prompt('Введіть тип шрифта');
+// document.body.style.fontFamily = fontStyle;
+
+// const levening = prompt('Вирівняйте заголовок');
+// content.style.textAlign = levening;
+
+// let content = document.getElementById('content')
+
+
+// let fonts = prompt('Введіть фон для параграфа')
+// links.style.backgroundColor = fonts;
+
+
+// let linksBg = prompt('Введіть колір ссилок')
+// for(let i = 0;i < links.children.length; i++){
+//     links.children[i].style.color= linksBg;
+// }
+
+// let divColor = prompt('Введіть колір для div')
+// let divSize = prompt('Введіть розмір шрифта для div')
+// let divWid = prompt('Введіть товщину тексту для div')
+// divCol.style.color = divColor;
+// divCol.style.fontSize = divSize;
+// divCol.style.width = divWid;
+
+
+// let mar = prompt('Введіть тип маркера');
+// mark.style.listStyleType = mar;
+
+
+
+
